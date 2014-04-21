dcb933a674d6319e976df8e4a138d878a23c9174
Gearlles <gearlles@gmail.com>
2014-04-21 11:16:18 -0300

    First commit

---
+++ fss-enhanced/src/main/java/com/gearlles/natural_computing/fss_enhanced/App.java
@@ -0,0 +1,13 @@
+package com.gearlles.natural_computing.fss_enhanced;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
